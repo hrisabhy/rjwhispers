951acbfe76a58ced41f941141f6978b3cc61efbc
hrisabhy <hrisabhyadav31@gmail.com>
2025-02-17 07:30:54 +0530

    comment route prisma client

--- app/api/confessions/route.ts
+++ app/api/confessions/route.ts
@@ -1,5 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
-import prisma from "@/db";
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 export async function POST(req: NextRequest) {
   try {
